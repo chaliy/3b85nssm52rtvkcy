314414a9c2daf06eca624508bff6e328d8079392
Mykhailo Chalyi <Mykhailo_Chalyi@external.mckinsey.com>
2021-12-27 23:48:06 +0200

    App1 more heavy grid

--- app1/src/app/components/heavy/heavy.component.ts
+++ app1/src/app/components/heavy/heavy.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {
+  GridReadyEvent,
+  FilterOpenedEvent,
+  FilterChangedEvent,
+  FilterModifiedEvent
+} from '@ag-grid-community/core'
 
 @Component({
   selector: 'app-heavy',
@@ -7,11 +14,95 @@ import { Component } from '@angular/core';
 })
 export class HeavyComponent {
 
-  columnDefs = [{ field: "make" }, { field: "model" }, { field: "price" }];
+  columnDefs;
+  defaultColDef;
+  rowData = [];
 
-  rowData = [
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxter", price: 72000 }
-  ];
+
+  constructor(private http: HttpClient) {
+
+    this.columnDefs = [
+      {
+        field: 'athlete',
+        filter: 'agTextColumnFilter',
+        filterParams: {
+          buttons: ['reset', 'apply'],
+        },
+      },
+      {
+        field: 'age',
+        maxWidth: 100,
+        filter: 'agNumberColumnFilter',
+        filterParams: {
+          buttons: ['apply', 'reset'],
+          closeOnApply: true,
+        },
+      },
+      {
+        field: 'country',
+        filter: 'agSetColumnFilter',
+        filterParams: {
+          buttons: ['clear', 'apply'],
+        },
+      },
+      {
+        field: 'year',
+        filter: 'agSetColumnFilter',
+        filterParams: {
+          buttons: ['apply', 'cancel'],
+          closeOnApply: true,
+        },
+        maxWidth: 100,
+      },
+      { field: 'sport' },
+      {
+        field: 'gold',
+        filter: 'agNumberColumnFilter',
+      },
+      {
+        field: 'silver',
+        filter: 'agNumberColumnFilter',
+      },
+      {
+        field: 'bronze',
+        filter: 'agNumberColumnFilter',
+      },
+      {
+        field: 'total',
+        filter: 'agNumberColumnFilter',
+      },
+    ];
+
+    this.defaultColDef = {
+      flex: 1,
+      minWidth: 150,
+      filter: true,
+    };
+  }
+
+  onFilterOpened(e: FilterOpenedEvent) {
+    console.log('onFilterOpened', e);
+  }
+
+  onFilterChanged(e: FilterChangedEvent) {
+    console.log('onFilterChanged', e);
+    console.log('gridApi.getFilterModel() =>', e.api.getFilterModel());
+  }
+
+  onFilterModified(e: FilterModifiedEvent) {
+    console.log('onFilterModified', e);
+    console.log('filterInstance.getModel() =>', e.filterInstance.getModel());
+    console.log(
+      'filterInstance.getModelFromUi() =>',
+      e.filterInstance.getModel()
+    );
+  }
+
+  onGridReady(params: GridReadyEvent) {
+
+    this.http
+      .get('https://www.ag-grid.com/example-assets/olympic-winners.json')
+      .subscribe((data) => params.api.setRowData(data as any[]));
+  }
+
 }
